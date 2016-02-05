941ff80982921b15c9b55c59918ed14d4f33ae1d
root <tanyameriamsunny@gmail.com>
2016-02-05 13:00:15 -0500

    added bootstrap and j query drop down

--- myuniversity/settings.py
+++ myuniversity/settings.py
@@ -115,8 +115,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (MEDIA_ROOT,)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATIC_URL = '/static/'
